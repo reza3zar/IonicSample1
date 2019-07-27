cb6833248886fb44e3d5f79fe8722422385a3a8e
Reza Younesi <younesi.nima@gmail.com>
2019-07-27 11:20:12 +0430

    refresh products

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,7 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { IonicStorageModule } from '@ionic/storage';
-
+import { IonRefreshNativeModule } from 'ion-refresh-native';
 @NgModule({
   declarations: [AppComponent,SystemNotifactionsPage],
   entryComponents: [SystemNotifactionsPage],
@@ -21,6 +21,7 @@ import { IonicStorageModule } from '@ionic/storage';
     IonicModule.forRoot(),
     IonicStorageModule.forRoot(),
     AppRoutingModule,
+    IonRefreshNativeModule,
     HttpModule
   ],
   schemas: [
--- src/app/products/products.page.ts
+++ src/app/products/products.page.ts
@@ -1,3 +1,4 @@
+import { ToastController } from '@ionic/angular';
 import { Product } from './../../Models/product';
 import { Component, OnInit } from '@angular/core';
 import { Observable, Subscription } from "rxjs";
@@ -10,29 +11,79 @@ import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 })
 export class ProductsPage implements OnInit {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private toastController:ToastController) { }
   public isLoadedData=false;
   public itemsFakes=[1,2,3,4,5,6,7,8]
   ngOnInit() {
 
     try {
-      this.getAllProducts().subscribe(result=>{
+      this.getTopProducts().subscribe(result=>{
         this.productCollection=result;
-      this.isLoadedData=true;
-
-      })
+        this.isLoadedData=true;
+      },
+      err =>{
+        this.errorConnectTOApi()
+        this.isLoadedData=true;
+      } ,
+      )
     } catch (error) {
-      console.log(error)
+        console.log(error)
     }
     finally{
+      
     }
 
   }
   productCollection=new Array<Product>();
 
 
-public getAllProducts(): Observable<Product[]>{
+public getTopProducts(): Observable<Product[]>{
+  try {
   return this.http.get<Product[]>('http://www.mocky.io/v2/5d386c509f0000ab4e9b3ef0');
+    
+  } catch (error) {
+console.log('123')
+    
+  }
+}
+
+doRefresh(event) {
+
+  this.getAllProducts().subscribe(result=>{
+    this.productCollection=result;
+    this.isLoadedData=true;
+    event.target.complete();
+  },
+  err =>{
+    this.errorConnectTOApi()
+    this.isLoadedData=true;
+    event.target.complete();
+  } ,)
+  //console.log('Begin async operation');
+  // setTimeout(() => {
+  //   console.log('Async operation has ended');
+  //   event.target.complete();
+  // }, 2000);
+}
+
+public getAllProducts(): Observable<Product[]>{
+  try {
+  return this.http.get<Product[]>('http://www.mocky.io/v2/5d3bf1c5300000bda6a2a160') ;
+    
+  } catch (error) {
+ 
+    
+  }
+}
+async errorConnectTOApi() {
+  const toast = await this.toastController.create({
+    header: 'هشدار',
+    message:'خطا در اتصال به سرویس، لطفا از دسترسی به اینترنت خود مطمئن شوید!',
+    position: 'bottom',
+    color: 'danger',
+    duration: 3000,
+  });
+  toast.present();
 }
 
 }
